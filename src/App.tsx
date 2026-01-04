86a41a8d380c2517c4c8bdb590ce9ed9ac89dc73
vaaniGO <vaanigoenka445@gmail.com>
2026-01-04 11:06:45 +0530

    new build

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,11 @@ const App = () => (
       <GridBackground />
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/vaani-goenka">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
